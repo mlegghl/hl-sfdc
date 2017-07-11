29e6e0313811a603097fcfe9eb4b99d3dfd9998d
Marcus Dillavou <dillavou@vipaar.com>
2017-07-11 16:10:16 -0500

    Make the duration nicer

--- metadata/aura/hlRecentEvent/hlRecentEventController.js
+++ metadata/aura/hlRecentEvent/hlRecentEventController.js
@@ -14,5 +14,27 @@
         } else {
             component.set("v.status", "In Progress");
         }
+
+        // figure out the duration
+        if (item.helplightning__Complete__c) {
+            if (item.helplightning__Duration__c > 60 * 60) {
+                // hours
+                var hours = Math.floor(item.helplightning__Duration__c / 60 / 60);
+                var minutes = Math.floor((item.helplightning__Duration__c / 60) - (hours * 60));
+                var seconds = item.helplightning__Duration__c - (hours * 60 * 60) - (minutes * 60);
+
+                component.set("v.duration", hours + 'h' + minutes + 'm' + seconds + 's');
+            } else if (item.helplightning__Duration__c > 60) {
+                var minutes = Math.floor(item.helplightning__Duration__c / 60);
+                var seconds = item.helplightning__Duration__c - (minutes * 60);
+
+                component.set("v.duration", minutes + 'm' + seconds + 's');
+            } else {
+                component.set("v.duration", item.helplightning__Duration__c + 's');
+            }
+        } else {
+            /* not complete yet */
+            component.set("v.duration", "");
+        }
 	}
 })

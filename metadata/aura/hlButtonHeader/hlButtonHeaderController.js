ab9b59d349aa1d12803c4dcac8eb1bd5f88e1614
Marcus Dillavou <dillavou@vipaar.com>
2017-07-17 08:46:20 -0500

    Removed some debug messages

--- metadata/aura/hlButtonHeader/hlButtonHeaderController.js
+++ metadata/aura/hlButtonHeader/hlButtonHeaderController.js
@@ -1,15 +1,11 @@
 ({
     clickCall : function(component, event, helper) {
-        console.log("hlButtonHeaderController::clickCall");
-
         // call the parent handler
         var event = component.getEvent('onCallClick');
         event.fire();
     },
 
     clickInviteToPersonalRoom : function(component, event, helper) {
-        console.log("hlButtonHeaderController::clickInviteToPersonalRoom");
-
         // call the parent handler
         var event = component.getEvent('onInviteClick');
         event.fire();
--- metadata/aura/hlSessions/hlSessionsController.js
+++ metadata/aura/hlSessions/hlSessionsController.js
@@ -1,6 +1,6 @@
 ({
     doInit : function(component, event, helper) {
-        console.log("doInit, revision 33r");
+        console.log("Initializing Help Lightning Component");
 
         var rId = component.get("v.recordId");
         var sObjectName = component.get("v.sObjectName");
@@ -38,14 +38,13 @@
                     } else if (component.isValid && state == "ERROR") {
                         component.set("v.hasErrors", true);
 
-                        console.log("setting error to: " + response.getError());
                         var errors = response.getError();
                         if (errors) {
                             if (errors[0] && errors[0].message) {
                                 component.set("v.errorMessage", errors[0].message);
                             }
                         } else {
-                            console.log("Unknown error during isHLUser");
+                            console.log("HL::isHLUser response failed: " + repsonse);
                         }
                     }
                 });
@@ -69,25 +68,20 @@
     },
 
     clickCall : function(component, event, helper) {
-        console.log("clickCall");
-
         var sObjectName = component.get("v.sObjectName");
         var rId = component.get("v.recordId");
         var contact = component.get("v.contact");
         var email = contact.Email;
-        console.log("email " + email);
 
         var action = component.get("c.makeSessionWith");
         action.setParams({"otherUsersEmail": email});
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (component.isValid() && state == "SUCCESS") {
-                console.log("response is " + response.getReturnValue());
-
                 // create a new HLCall
                 helper.createNewCall(component, sObjectName, rId, email, response.getReturnValue());
             } else {
-                console.log("response failed: " + state);
+                console.log("HL::makeSessionWith response failed: " + state);
             }
         });
 
@@ -95,8 +89,6 @@
     },
 
     clickInviteToPersonalRoom : function(component, event, helper) {
-        console.log("clickInviteToPersonalRoom");
-
         var contact = component.get("v.contact");
         var email = contact.Email;
         var name = contact.Name;
@@ -110,7 +102,7 @@
 
                 // !mwd - show a status update about this
             } else {
-                console.log("failed to invite to personal room: " + state);
+                console.log("HL::inviteToPersonalRoom response failed: " + state);
             }
         });
 

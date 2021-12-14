730d2d0e73ea703a79cd69a52b3d0d9ea3d0fbf9
Micah Legg <micahlegg@Micah-Leggs-MacBook-Pro.local>
2021-12-14 08:49:51 -0600

    add gif video to help tab

--- hlsfdc/main/default/lwc/hlHelp/hlHelp.js
+++ hlsfdc/main/default/lwc/hlHelp/hlHelp.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
+import HL_GIF from '@salesforce/resourceUrl/rvalow';
 
 export default class HlContacts extends LightningElement {
+    hlGifUrl = HL_GIF;
     helpResourceUrl = 'https://helplightning.com/support/';
 }

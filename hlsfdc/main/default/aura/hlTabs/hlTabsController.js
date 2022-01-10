18ab891457aa068d81e63d45e597d357e4933ce6
Micah Legg <micahlegg@Micah-Leggs-MacBook-Pro.local>
2022-01-10 10:22:43 -0600

    removed contact requirement, improved error handling

--- hlsfdc/main/default/aura/hlTabs/hlTabsController.js
+++ hlsfdc/main/default/aura/hlTabs/hlTabsController.js
@@ -1,8 +1,10 @@
 ({
     doInit : function(component, event, helper) {
-        // to prevent 2 way binding issues, set another attribute with the default phone number if it exists
+        // to prevent 2 way binding issues, set another attribute with the default phone and email if it exists
         var phone = component.get("v.defaultPhone");
+        var email = component.get("v.defaultEmail");
         component.set("v.contactPhone", phone);
+        component.set("v.contactEmail", email);
     },
     clickCall : function(component, event, helper) {
         // get contact email from contacts call handler
@@ -17,13 +19,24 @@
     sendInvite : function(component, event, helper) {
         // call the parent handler
         var event = component.getEvent('onInviteClick');
-        var email = component.find('contactEmail').get('v.value');
-        var phone = component.find('contactPhone').get('v.value') || '';
+        var phone = component.get("v.contactPhone") || '';
+        var email = component.get("v.contactEmail");
         var message = component.get('v.inviteMessage');
 
-        event.setParam("email", email);
-        event.setParam("phone", phone.replace(/[^+\d]/g, ''));
-        event.setParam("message", message);
-        event.fire();
+        if (email == '' && phone == '') {
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "type": "warning",
+                "message": "Please enter a valid email or phone."
+            });
+            toastEvent.fire();
+        } else {
+            event.setParam("email", email);
+            event.setParam("phone", phone.replace(/[^+\d]/g, ''));
+            event.setParam("message", message);
+            event.fire();
+        }
+
+        
     }
 })

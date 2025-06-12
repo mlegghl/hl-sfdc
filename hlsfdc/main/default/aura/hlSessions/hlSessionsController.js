f44f7184777cdd69a46eee11a144fe34311a7fc0
Micah Legg <micah.legg@helplightning.com>
2025-06-12 07:45:14 -0500

    Merge change from master, add error handling for clipboard func

--- hlsfdc/main/default/aura/hlSessions/hlSessionsController.js
+++ hlsfdc/main/default/aura/hlSessions/hlSessionsController.js
@@ -157,14 +157,21 @@
                 var auth = r?.auth;
 
                 // copy the mhs link to the clipboard
-                navigator.clipboard.writeText(link.longLink);
-
-                var toastEvent = $A.get("e.force:showToast");
-                toastEvent.setParams({
-                    "type": "success",
-                    "message": "Invite link copied to the clipboard."
+                navigator.clipboard.writeText(link.longLink).then(function() {
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "type": "success",
+                        "message": "Invite link copied to the clipboard."
+                    });
+                    toastEvent.fire();
+                }).catch(function() {
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "type": "error",
+                        "message": "Failed to copy invite link to clipboard."
+                    });
+                    toastEvent.fire();
                 });
-                toastEvent.fire();
 
                 var webUrl = auth.webUrl;
                 var userToken = auth.token;
